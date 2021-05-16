8d58c6d8cdd6ffa10e53c8818e0ffa0690708d74
msn2106 <msn2106@gmail.com>
2021-05-16 22:43:59 +0530

    searching algos(linear+binary+jump)

--- src/searching/searchBinaryArrayInt.java
+++ src/searching/searchBinaryArrayInt.java
@@ -1,6 +1,10 @@
 /* Binary Search works only on sorted array. */
 package searching;
 
+/**
+ * Time Complexity - T(n) = T(n/2) + c - theta(logn)
+ * Space - O(logn) - recursive(recursive call stack) , O(1) iterative
+ */
 public class searchBinaryArrayInt {
 
 	public static void main(String[] args) {
@@ -8,23 +12,34 @@ public class searchBinaryArrayInt {
 		int [] arr = { 2,5,9,13,14,19,24,28,37,42,56,88,94};
 		
 		int item = 19;
-		int lowerIndex = 0;
-		int higherIndex = arr.length -1;
-		int middleIndex = (lowerIndex+higherIndex)/2;
-		
-		while(lowerIndex <= higherIndex)
-		{
-			if(arr[middleIndex]==item)
-			{
-				System.out.println(item+" is at index "+middleIndex+" in the array.");
-				break;
-			}
-			else if(arr[middleIndex]<item) lowerIndex = middleIndex + 1;
-			else higherIndex = middleIndex -1;
-			middleIndex = (lowerIndex+higherIndex)/2;
-		}
-		if(middleIndex > higherIndex) System.out.println("element not found");
-
+		int index = binarySearch1(arr, 0, arr.length-1, item);
+		if(index == 0) System.out.println(item+" is not present");
+		else System.out.println(item+" is present at index "+ index);
 	}
 
+	//binarySearch - recursive
+	public static int binarySearch(int[] a, int l, int r, int k){
+		if( l <= r){
+			int mid = l + (r-l)/2;
+			if(a[mid] == k) return mid;
+			if(a[mid] > k) return binarySearch(a,l,mid-1,k);
+			return binarySearch(a,mid+1,r,k);
+		}
+		return -1;
+	}
+
+	//binarySearch - iterative
+	public static int binarySearch1(int[] a, int l, int r, int k){
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+
+			// Check if x is present at mid
+			if (a[m] == k) return m;
+			// If x greater, ignore left half
+			if (a[m] < k)l = m + 1;
+			// If x is smaller, ignore right half
+			else r = m - 1;
+		}
+		return -1;
+	}
 }
